d425613d7a2d5253cd68b5f2678d760f4a25bcd5
Timo Stalschus <stalschus1998@gmx.declear>
2026-03-03 23:52:08 +0100

    listen lassen sich importieren und anzeigen

--- flask-projekt/app.py
+++ flask-projekt/app.py
@@ -142,16 +142,16 @@ def register():
 
 @app.route("/open_list",methods=["POST","GET"])
 def open_list():
-   data = request.get_json()
-   session["list_id"] = data["listenid"]  
-   return jsonify({"status":"ok"}) 
+   if "user_id" in session: #abfangen von wieder in die liste kommen nach loggout(geht nicht. zurückknopf keine neue request)
+    data = request.get_json()
+    session["list_id"] = data["listenid"]  
+    return jsonify({"status":"ok"}) 
 
 
 
 @app.route("/open_list_page",methods=["POST","GET"])
 def open_list_page():
    
-   print("I'm Here :)")
    listen_id = session["list_id"]
    conn = sqlite3.connect("todo.db")
    conn.row_factory = sqlite3.Row
@@ -169,6 +169,69 @@ def open_list_page():
    return  render_template("listen_einsicht.html", todos=todos)
 
 
+@app.route("/listen.html",methods=["POST","GET"])
+def lists():
+   if request.method=="GET":
+      conn = sqlite3.connect("todo.db")
+      conn.row_factory = sqlite3.Row
+      cursor = conn.cursor()
+
+      cursor.execute("""SELECT Listen.ListenID, Listen.Listenname 
+                        FROM userLists, Listen
+                        WHERE userLists.UserID = ?
+                        AND userLists.ListenID = Listen.ListenID"""
+                     ,(session["user_id"],))
+      listen = cursor.fetchall()
+      conn.close()
+      return jsonify([dict(liste) for liste in listen])
+   
+@app.route("/logout",methods=["POST","GET"])
+def logout(): 
+    if request.method == "GET":
+       session.clear() 
+       
+       return render_template("login.html")
+    
+@app.route("/backward",methods=["POST","GET"])
+def backward():
+   if "user_id" in session:
+      return render_template("listen.html")
+   return render_template("login.html")
+
+@app.route("/add_list_with_id",methods=["GET","POST"])
+def add_list_with_id():
+   data = request.get_json()
+   listen_id = data["listenid"]
+   conn = sqlite3.connect("todo.db")
+   conn.row_factory = sqlite3.Row
+   cursor = conn.cursor()
+   cursor.execute("""
+        INSERT INTO userLists (UserID, ListenID)
+        VALUES (?,?)
+    """,(session["user_id"],listen_id,))
+   conn.commit()
+   neue_Liste = cursor.execute("""
+        SELECT Listenname, ListenID
+        FROM Listen
+        WHERE ListenID = ?
+    """,(listen_id,)).fetchone()
+   print(dict(neue_Liste))
+   conn.close()
+   return jsonify([dict(neue_Liste)]) 
+
+
+@app.route("/load_list",methods=["GET","POST"])
+def load_list():
+   data = request.get_json()
+   listen_id = data["listenid"]
+   conn = sqlite3.connect("todo.db")
+   conn.row_factory = sqlite3.Row
+   cursor = conn.cursor()
+   liste = cursor.execute("""
+        SELECT ListenID,Listenname FROM Listen WHERE ListenID = ?
+""",(listen_id,)).fetchone()
+   conn.close()
+   return [dict(liste)]
 init_db()  
 if __name__ == "__main__":
     app.run(debug=True)
